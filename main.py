7f810fe7b4b66af7bd5007a61e1b1107d97e85ba
christradebot <chrisgroeneweg78@gmail.com>
2026-07-05 11:18:58 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, request, jsonify
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 
 from alpaca.trading.client import TradingClient
 from alpaca.trading.requests import StopLimitOrderRequest
